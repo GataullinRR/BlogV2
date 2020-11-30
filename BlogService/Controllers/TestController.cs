7c8a21697801d5891ccd0f6203f2ee6abe4d2ed7
Radmir <QTU100@gmail.com>
2020-11-30 23:21:54 +0500

    + test controller

---
+++ BlogService/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public IActionResult Health()
+        {
+            return Content("Healthy!");
+        }
+    }
+}
