4e715a66827c9293a72e0df1e4fccaeb97184e6b
Radmir <QTU100@gmail.com>
2020-11-30 22:43:55 +0500

    Lol. Fixed url to blog service

--- Blog.API/Misc/BlogOptions.cs
+++ Blog.API/Misc/BlogOptions.cs
@@ -6,6 +6,6 @@ namespace BlogService.API
     public class BlogServiceOptions
     {
         [Required]
-        public Uri Address { get; set; } = new Uri("https://localhost:5010");
+        public Uri Address { get; set; } = new Uri("http://ec2-35-181-48-229.eu-west-3.compute.amazonaws.com:80/");
     }
 }
